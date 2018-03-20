8bf60c011331d7dd90ed6f6b42ab926b066c4332
Pavel Pokalnis <pavel.pokalnis@monterosa.co.uk>
2018-03-20 15:20:16 +0300

    Init

---
+++ jplib/src/main/java/no/domain/jplib/PrintManager.java
@@ -0,0 +1,14 @@
+package no.domain.jplib;
+
+import android.util.Log;
+
+/**
+ * Created by pokalnis_p on 20.03.2018.
+ */
+
+public class PrintManager {
+
+	public static void print() {
+		Log.e(PrintManager.class.getSimpleName(), "Hello from lib");
+	}
+}
